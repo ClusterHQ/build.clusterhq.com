8fba9ab604e87a0bed46ab1f9ea7a7d8ba4715f9
Tom Prince <tom.prince@clusterhq.com>
2014-11-13 22:05:09 -0700

    Wait for image to be copied, as well.

--- slave/common.py
+++ slave/common.py
@@ -1,4 +1,5 @@
 import yaml
+import time
 from libcloud.compute.providers import get_driver, Provider
 
 aws_config = yaml.safe_load(open("aws_config.yml"))
@@ -7,3 +8,18 @@ driver = get_driver(Provider.EC2)(
     key=aws_config['access_key'],
     secret=aws_config['secret_access_token'],
     region=aws_config['region'])
+
+def wait_for_image(image):
+    """
+    Wait for an image to be available.
+    """
+    while True:
+      try:
+        while driver.get_image(image.id).extra['state'] != 'available':
+            time.sleep(1)
+        else:
+            return
+      except IndexError:
+        time.sleep(1)
+      except:
+          raise
