1e357e394e94117d19a15467c3b579749e827e70
Tom Prince <tom.prince@clusterhq.com>
2015-01-11 08:10:17 -0700

    Wait to start builds, so that they are distributed.

--- flocker_bb/monkeypatch.py
+++ flocker_bb/monkeypatch.py
@@ -1,6 +1,7 @@
 from twisted.internet import defer
 from buildbot.process import metrics
 from twisted.python import log
+from twisted.internet import reactor
 
 
 @defer.inlineCallbacks
@@ -43,7 +44,25 @@ def brd_activityLoop(self):
     self._quiet()
 
 
+def botmaster_maybeStartBuildsForSlave(self, slave_name):
+    """
+    We delay this for 10 seconds, so that if multiple slaves start at the same
+    time, builds will be distributed between them.
+    """
+    def do_start():
+        log.msg(format="Really starting builds on %(slave_name)s",
+                slave_name=slave_name)
+        builders = self.getBuildersForSlave(slave_name)
+        self.brd.maybeStartBuildsOn([b.name for b in builders])
+    log.msg(format="Waiting to start builds on %(slave_name)s",
+            slave_name=slave_name)
+    reactor.callLater(10, do_start)
+
+
 def apply_patches():
+    log.msg("Apply flocker_bb.monkeypatch.")
     from buildbot.process.buildrequestdistributor import (
         BuildRequestDistributor)
     BuildRequestDistributor._activityLoop = brd_activityLoop
+    from buildbot.process.botmaster import BotMaster
+    BotMaster.maybeStartBuildsForSlave = botmaster_maybeStartBuildsForSlave
