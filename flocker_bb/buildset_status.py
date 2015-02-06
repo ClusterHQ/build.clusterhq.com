a899a73634b130d5f93331bef9b7b2e3e6c0004b
Tom Prince <tom.prince@clusterhq.com>
2015-02-06 16:35:45 -0700

    lint

--- flocker_bb/buildset_status.py
+++ flocker_bb/buildset_status.py
@@ -17,7 +17,6 @@ class BuildsetStatusReceiver(StatusReceiverService):
         self.finished = finished
         self._builders = []
 
-
     def startService(self):
         StatusReceiverService.startService(self)
         self.status = self.parent
@@ -26,7 +25,6 @@ class BuildsetStatusReceiver(StatusReceiverService):
         # Get builderAdded notification
         self.status.subscribe(self)
 
-
     def stopService(self):
         self.status.unsubscribe(self)
         for builder in self._builders:
@@ -34,23 +32,24 @@ class BuildsetStatusReceiver(StatusReceiverService):
 
         return StatusReceiverService.stopService(self)
 
-
     def _buildsetData(self, buildset):
         """
         @returns: L{tuple} of sourcestamp dicts and buildrequest dicts.
         """
         dl = []
-        dl.append(self.master.db.sourcestamps.getSourceStamps(buildset.bsdict['sourcestampsetid']))
+        dl.append(
+            self.master.db.sourcestamps.getSourceStamps(
+                buildset.bsdict['sourcestampsetid']))
         dl.append(buildset.getBuilderNamesAndBuildRequests()
-                .addCallback(lambda res: gatherResults([br.asDict_async() for br in res.values()])))
+                  .addCallback(
+                      lambda res: gatherResults([br.asDict_async()
+                                                 for br in res.values()])))
         return gatherResults(dl)
-
 
     def buildsetFinished(self, buildset):
         d = self._buildsetData(buildset)
         d.addCallback(self.finished, self.parent)
         d.addErrback(err, "BuildsetStatusReceiver.buildsetFinished")
-
 
     def buildsetSubmitted(self, buildset):
         if self.finished:
