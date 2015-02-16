7d544bd23b973437ae8efd6625ea43e0069b255e
Tom Prince <tom.prince@clusterhq.com>
2015-02-16 12:59:48 -0700

    Add script to set PATH.
    http://www.openradar.me/18945659

--- slave/osx/fabfile.py
+++ slave/osx/fabfile.py
@@ -20,8 +20,10 @@ def create_user(username, uid=650):
 
 @task
 def install(index, password, master='build.staging.clusterhq.com'):
+    slave_home = FilePath('/Users/buildslave')
+
     create_user('buildslave')
-    with settings(sudo_user='buildslave', shell_env={'HOME': '/Users/buildslave'}), cd('/Users/buildslave'):  # noqa
+    with settings(sudo_user='buildslave', shell_env={'HOME': slave_home.path}), cd(slave_home.path):  # noqa
         sudo("curl -O https://bootstrap.pypa.io/get-pip.py")
         sudo("python get-pip.py --user")
         sudo("~/Library/Python/2.7/bin/pip install --user buildbot-slave==0.8.10 virtualenv==12.0.7")  # noqa
@@ -31,4 +33,7 @@ def install(index, password, master='build.staging.clusterhq.com'):
 
     put(FilePath(__file__).sibling('launchd.plist').path,
         '/Library/LaunchDaemons/net.buildslave.plist', use_sudo=True)
+    put(FilePath(__file__).sibling('start-buildslave').path,
+        slave_home.child('flocker-osx').child('start-buildslave').path,
+        mode=0755, use_sudo=True)
     sudo('chown root:wheel /Library/LaunchDaemons/net.buildslave.plist')
