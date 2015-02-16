6df7208a6fd1d461b5c4adb39580130d94c7c6a0
Tom Prince <tom.prince@clusterhq.com>
2015-02-16 14:51:42 -0700

    Use different S3 URL.

--- slave/vagrant/fabfile.py
+++ slave/vagrant/fabfile.py
@@ -42,11 +42,6 @@ ARCH=$(uname -m)
 yum install -y https://kojipkgs.fedoraproject.org//packages/kernel/${KV}/${SV}/${ARCH}/kernel-devel-${UNAME_R}.rpm  # noqa
 """)
 
-    run("""
-ARCH=$(uname -m)
-yum install -y https://clusterhq.s3.amazonaws.com/repo/fedora/20/${ARCH}/phantomjs-1.9.8-1.x86_64.rpm  # noqa
-""")
-
     run('yum install -y dkms')
     packages = [
         "https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.5_x86_64.rpm",
@@ -59,6 +54,7 @@ yum install -y https://clusterhq.s3.amazonaws.com/repo/fedora/20/${ARCH}/phantom
         "python-devel",
         "python-virtualenv",
         "openssl-devel",
+        "https://clusterhq.s3.amazonaws.com/phantomjs-fedora-20/phantomjs-1.9.8-1.x86_64.rpm",  # noqa
     ]
     run("yum install -y " + " ".join(packages))
     run("useradd buildslave")
