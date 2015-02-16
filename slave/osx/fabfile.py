f93705f1bc0086793ac8667f78594cc80f82a5b1
Tom Prince <tom.prince@clusterhq.com>
2015-02-16 12:19:19 -0700

    Start slave on boot.

--- slave/osx/fabfile.py
+++ slave/osx/fabfile.py
@@ -1,4 +1,5 @@
-from fabric.api import sudo, task, settings, cd
+from fabric.api import sudo, task, settings, cd, put
+from twisted.python.filepath import FilePath
 
 
 @task
@@ -20,10 +21,14 @@ def create_user(username, uid=650):
 @task
 def install(index, password, master='build.staging.clusterhq.com'):
     create_user('buildslave')
-    with settings(sudo_user='buildslave', shell_env={'HOME': '/Users/buildslave'}), cd('/Users/buildslave'):
+    with settings(sudo_user='buildslave', shell_env={'HOME': '/Users/buildslave'}), cd('/Users/buildslave'):  # noqa
         sudo("curl -O https://bootstrap.pypa.io/get-pip.py")
         sudo("python get-pip.py --user")
         sudo("~/Library/Python/2.7/bin/pip install --user buildbot-slave==0.8.10 virtualenv==12.0.7")  # noqa
 
         sudo("~/Library/Python/2.7/bin/buildslave create-slave ~/flocker-osx %(master)s osx-%(index)s %(password)s"  # noqa
              % {'index': index, 'password': password, 'master': master})
+
+    put(FilePath(__file__).sibling('launchd.plist').path,
+        '/Library/LaunchDaemons/net.buildslave.plist', use_sudo=True)
+    sudo('chown root:wheel /Library/LaunchDaemons/net.buildslave.plist')
