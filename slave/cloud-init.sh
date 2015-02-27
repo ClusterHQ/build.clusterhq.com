8c8aa4c38bafa87f15693bdce1a171cb678fc363
Tom Prince <tom.prince@clusterhq.com>
2015-02-27 16:11:25 -0700

    Set umask for debian packages.

--- slave/cloud-init.sh
+++ slave/cloud-init.sh
@@ -6,7 +6,8 @@ if [[ "%(base)s" =~ (fedora|centos)-zfs-head ]]; then
    systemctl restart zfs.target
 fi
 
-buildslave create-slave /srv/buildslave %(buildmaster_host)s:%(buildmaster_port)d '%(name)s' '%(password)s'
+# We set umask so that package builds get the right permissions. (Lintian will catch us if we get it wrong).
+buildslave create-slave --umask 022 /srv/buildslave %(buildmaster_host)s:%(buildmaster_port)d '%(name)s' '%(password)s'
 
 # Set $HOME so that git has a config file to read.
 export HOME=/srv/buildslave
