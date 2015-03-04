ee0fcc54d2d96bec71ce66eb98a66dd909439436
Tom Prince <tom.prince@clusterhq.com>
2015-03-03 19:53:45 -0700

    Add running and finished build metrics.

--- flocker_bb/monitoring.py
+++ flocker_bb/monitoring.py
@@ -1,12 +1,12 @@
-from collections import Counter
 
 from twisted.application.internet import TimerService
 from twisted.internet.defer import inlineCallbacks, returnValue
 from twisted.python import log
 
 from buildbot.status.base import StatusReceiverMultiService
+from buildbot.status.results import Results
 
-from prometheus_client import Gauge
+from prometheus_client import Gauge, Counter
 
 
 class Monitor(StatusReceiverMultiService):
@@ -15,6 +15,20 @@ class Monitor(StatusReceiverMultiService):
         'pending_builds_total',
         'Number of pending builds',
         labelnames=['builder'],
+        namespace='buildbot',
+    )
+
+    building_counts_gauge = Gauge(
+        'running_builds_total',
+        'Number of running builds',
+        labelnames=['builder', 'slave_class', 'slave_number'],
+        namespace='buildbot',
+    )
+
+    build_counts = Counter(
+        'finished_builds_total',
+        'Number of finished builds',
+        labelnames=['builder', 'slave_class', 'slave_number', 'result'],
         namespace='buildbot',
     )
 
@@ -33,6 +47,7 @@ class Monitor(StatusReceiverMultiService):
 
     @inlineCallbacks
     def count_pending_builds(self):
+        from collections import Counter
         build_reqs = yield self.master.db.buildrequests.getBuildRequests(
             claimed=False)
         pending_counts = Counter([br['buildername'] for br in build_reqs])
@@ -59,3 +74,38 @@ class Monitor(StatusReceiverMultiService):
             message += ['|%s|%d|' % item for item in pending_counts.items()]
             log.msg('\n'.join(message),
                     zulip_subject="Too Many Pending Builds")
+
+    def builderAdded(self, builderName, builder):
+        """
+        Notify this receiver of a new builder.
+
+        :return StatusReceiver: An object that should get notified of events on
+            the builder.
+        """
+        return self
+
+    def buildStarted(self, builderName, build):
+        """
+        Notify this receiver that a build has started.
+
+        Reports to github that a build has started, along with a link to the
+        build.
+        """
+        slave_name, slave_number = build.getSlavename().rsplit('-', 1)
+        self.building_counts_gauge.labels(
+            builderName, slave_name, slave_number).inc()
+
+    def buildFinished(self, builderName, build):
+        """
+        Notify this receiver that a build has started.
+
+        Reports to github that a build has started, along with a link to the
+        build.
+        """
+        slave_name, slave_number = build.getSlavename().rsplit('-', 1)
+        self.building_counts_gauge.labels(
+            builderName, slave_name, slave_number,
+        ).dec()
+        self.build_counts.labels(
+            builderName, slave_name, slave_number, Results[build.getResults()],
+        ).inc()
