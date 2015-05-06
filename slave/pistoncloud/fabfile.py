efbc36a4ae3e66941bf6b7191011be0f1e163bd0
Richard Wall <richard.wall@clusterhq.com>
2015-05-06 09:47:36 -0400

    Use the vagrant / rackspace fabfile as a template instead.

--- slave/pistoncloud/fabfile.py
+++ slave/pistoncloud/fabfile.py
@@ -1,47 +1,49 @@
-from fabric.api import sudo, task, settings, cd, put
+# Copyright Hybrid Logic Ltd.
+"""
+Configuration for a buildslave to run on PistonCloud
+
+.. warning::
+    This points at the staging buildserver by default.
+"""
+
+from fabric.api import run, task, sudo, put
 from twisted.python.filepath import FilePath
+from StringIO import StringIO
+import yaml
 
 
-@task
-def create_user(username, uid=650):
-    """
-    Create an OS X user.
-
-    UID must be > 500 for the user to show up in
-    /System/Library/PreferencePanes/Accounts.prefPane.
-
-    UID cannot conflict with an existing user.
-    UID and PrimaryGroupID were chosen to mimic a created standard user on a
-    test Mac.
-    The PrimaryGroupID is for the "staff" group.
-    """
-    sudo("""
-    dscl . -create /Users/%(username)s
-    dscl . -create /Users/%(username)s UserShell /bin/bash
-    dscl . -create /Users/%(username)s RealName "ClusterHQ Buildslave"
-    dscl . -create /Users/%(username)s UniqueID "%(uid)s"
-    dscl . -create /Users/%(username)s PrimaryGroupID 20
-    dscl . -create /Users/%(username)s NFSHomeDirectory /Users/%(username)s
-    createhomedir -c %(username)s
-    """ % {'username': username, 'uid': uid})
+def configure_acceptance():
+    put(StringIO(yaml.safe_dump({'metadata': {'creator': 'buildbot'}})),
+        '/home/buildslave/acceptance.yml')
 
 
 @task
 def install(index, password, master='build.staging.clusterhq.com'):
-    slave_home = FilePath('/Users/buildslave')
+    """
+    Install a buildslave for running cinder block device API tests against
+    Rackspace OpenStack API.
+    """
+    packages = [
+        "https://kojipkgs.fedoraproject.org//packages/buildbot/0.8.10/1.fc22/noarch/buildbot-slave-0.8.10-1.fc22.noarch.rpm",  # noqa
+        "git",
+        "libffi-devel",
+        "python",
+        "python-devel",
+        "python-virtualenv",
+        "openssl-devel",
+    ]
+    run("yum install -y " + " ".join(packages))
+    run("useradd buildslave")
+    sudo("buildslave create-slave /home/buildslave/rackspace_blockdevice %(master)s rackspace-blockdevice-%(index)s %(password)s"  # noqa
+         % {'index': index, 'password': password, 'master': master},
+         user='buildslave')
+    put(FilePath(__file__).sibling('start').path,
+        '/home/buildslave/rackspace_blockdevice/start', mode=0755)
 
-    create_user('buildslave')
-    with settings(sudo_user='buildslave', shell_env={'HOME': slave_home.path}), cd(slave_home.path): # noqa
-        sudo("curl -O https://bootstrap.pypa.io/get-pip.py")
-        sudo("python get-pip.py --user")
-        sudo("~/Library/Python/2.7/bin/pip install --user buildbot-slave==0.8.10 virtualenv==12.1.1")  # noqa
+    configure_acceptance()
 
-        sudo("~/Library/Python/2.7/bin/buildslave create-slave ~/flocker-osx %(master)s osx-%(index)s %(password)s"  # noqa
-             % {'index': index, 'password': password, 'master': master})
+    put(FilePath(__file__).sibling('rackspace-blockdevice-slave.service').path,
+        '/etc/systemd/system/rackspace-blockdevice-slave.service')
 
-    put(FilePath(__file__).sibling('launchd.plist').path,
-        '/Library/LaunchDaemons/net.buildslave.plist', use_sudo=True)
-    put(FilePath(__file__).sibling('start-buildslave').path,
-        slave_home.child('flocker-osx').child('start-buildslave').path,
-        mode=0755, use_sudo=True)
-    sudo('chown root:wheel /Library/LaunchDaemons/net.buildslave.plist')
+    run('systemctl start rackspace-blockdevice-slave')
+    run('systemctl enable rackspace-blockdevice-slave')
