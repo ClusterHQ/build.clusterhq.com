c7dcfb933bc58a9a57c112810c6e5d47d453cc8f
Tom Prince <tom.prince@clusterhq.com>
2015-05-07 18:10:05 -0600

    Grab availability zone of slave explicitly.

--- slave/cloud-init.sh
+++ slave/cloud-init.sh
@@ -26,6 +26,7 @@ export FLOCKER_FUNCTIONAL_TEST_CLOUD_CONFIG_FILE=$HOME/acceptance.yml
 
 # This will need to change if we ever have latent slaves on non-AWS.
 export FLOCKER_FUNCTIONAL_TEST_CLOUD_PROVIDER=aws
+export FLOCKER_FUNCTIONAL_TEST_AWS_AVAILABILITY_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
 
 touch /root/.ssh/known_hosts
 cat <<"EOF"  > /root/.ssh/id_rsa
