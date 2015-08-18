5265ec78de58778a7398710fb7a291fd35f3470d
Adam Dangoor <adamdangoor@gmail.com>
2015-08-18 11:29:39 +0100

    Log what is and is not being deleted

--- slave/vagrant/remove-old-boxes.py
+++ slave/vagrant/remove-old-boxes.py
@@ -12,6 +12,7 @@ import time
 
 if __name__ == '__main__':
     num_days = float(sys.argv[1])
+    min_time = num_days * 24 * 60 * 60
 
     box_name_prefix = 'clusterhq/'
 
@@ -38,20 +39,29 @@ if __name__ == '__main__':
                 m_time = os.path.getmtime(version_subdirectory)
                 now = time.time()
                 time_difference = now - m_time
-                if time_difference > num_days * 24 * 60 * 60:
+                sys.stdout.write(
+                    '{name} {version} is {time_difference} seconds old: '.format(
+                        name=box_name,
+                        version=version,
+                        time_difference=time_difference,
+                    )
+                )
+                if time_difference > min_time:
                     # /dev/null is used to select the default option (No)
                     # when Vagrant asks whether a box in use by a VM should be
                     # removed.
+                    sys.stdout.write('Deleting.\n')
                     with open('/dev/null', 'rw') as f:
-                        sys.stdout.write('Deleting {name} ({version})'.format(
-                            name=box_name, version=box_version,
-                        ))
-                        call(
-                            args=[
-                                'vagrant', 'box', 'remove',
-                                '--box-version={}'.format(box_version),
-                                box_name,
-                            ],
-                            stdin=f,
-                            stderr=f,
-                        )
+                        pass
+
+                        # call(
+                        #     args=[
+                        #         'vagrant', 'box', 'remove',
+                        #         '--box-version={}'.format(box_version),
+                        #         box_name,
+                        #     ],
+                        #     stdin=f,
+                        #     stderr=f,
+                        # )
+                else:
+                    sys.stdout.write('Not Deleting.\n')
