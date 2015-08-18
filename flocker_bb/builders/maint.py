9e12c6dd684d7116df1683ae407dbd1032927cc3
Adam Dangoor <adamdangoor@gmail.com>
2015-08-18 12:09:05 +0100

    Delete boxes only after 14 days

--- flocker_bb/builders/maint.py
+++ flocker_bb/builders/maint.py
@@ -58,7 +58,7 @@ def makeCleanOldBuildsFactory():
     # cleanup old boxes here. "Old" is the number of days passed as
     # parameter to script.
     factory.addStep(ShellCommand(
-        command=['python', '/home/buildslave/remove-old-boxes.py', '0.1'],
+        command=['python', '/home/buildslave/remove-old-boxes.py', '14'],
         description=['Removing', 'old', 'boxes'],
         descriptionDone=['Remove', 'old', 'boxes'],
         name='remove-old-boxes'))
